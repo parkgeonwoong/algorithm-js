acac21657ddf456ae0ab8e571c4aa08d59ee8fd9
parkgeonwoong <hk77kh@gmail.com>
2023-03-21 16:00:36 +0900

    Feat : ~보이는 학생

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
-let str = ["good", "time", "good", "time", "student"];
+let arr = [130, 135, 148, 140, 145, 150, 150, 153];
 
-function solution(str) {
-  let answer;
+function solution(arr) {
+  let answer = 1;
+  let max = arr[0];
 
-  answer = str.filter((v, i) => str.indexOf(v) === i);
+  for (let i = 1; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+      answer++;
+    }
+  }
 
-  return answer.join(" ");
+  return answer;
 }
 
-console.log(solution(str));
+console.log(solution(arr));
