b9d0466196cdc17d67f2b8314f52b85a75e7f1fd
parkgeonwoong <hk77kh@gmail.com>
2023-03-28 20:48:11 +0900

    Feat : 공통원소구하기 (투포인트)

--- codingtest/efficiency/02공통원소구하기.js
+++ codingtest/efficiency/02공통원소구하기.js
@@ -1,10 +1,37 @@
 /**
  * A, B 두 개의 집합이 주어지면 두 집합의 공통 원소를 추출하여 오름차순으로 출력하는 프로그램을 작성하세요.
+ *
+ * @로직
+ * 투포인트 알고리즘 구현
+ * 1. 정렬을 한다
+ * 2. 비교해서 같으면 배열에 담고, 두 배열의 인덱스 앞으로 이동
+ * 3. 다르면, 작은값이 인덱스 앞으로 이동
  */
-
+// 내가 푼 방법
 function solution(a, b) {
   let answer = a.filter((v) => b.includes(v));
   answer.sort((a, b) => a - b);
+  return answer;
+}
+
+// 투 포인트 알고리즘
+function solution(a, b) {
+  let answer = [];
+  let p1 = (p2 = 0);
+  a.sort((a, b) => a - b);
+  b.sort((a, b) => a - b);
+  const n = a.length;
+  const m = b.length;
+
+  while (p1 < n && p2 < m) {
+    if (a[p1] === b[p2]) {
+      answer.push(a[p1]);
+      p1++;
+      p2++;
+    } else if (a[p1] < b[p2]) p1++;
+    else if (a[p1] > b[p2]) p2++;
+  }
+
   return answer;
 }
 
--- test.js
+++ test.js
@@ -1,21 +1,24 @@
 function solution(a, b) {
   let answer = [];
+  let p1 = (p2 = 0);
+  a.sort((a, b) => a - b);
+  b.sort((a, b) => a - b);
   const n = a.length;
   const m = b.length;
-  let p1 = (p2 = 0);
 
   while (p1 < n && p2 < m) {
-    if (a[p1] <= b[p2]) answer.push(a[p1++]);
-    else answer.push(b[p2++]);
+    if (a[p1] === b[p2]) {
+      answer.push(a[p1]);
+      p1++;
+      p2++;
+    } else if (a[p1] < b[p2]) p1++;
+    else if (a[p1] > b[p2]) p2++;
   }
-
-  while (p1 < n) answer.push(a[p1++]);
-  while (p2 < m) answer.push(b[p2++]);
 
   return answer;
 }
 
-let a = [1, 3, 5];
-let b = [2, 3, 6, 7, 9];
+let a = [1, 3, 9, 5, 2];
+let b = [3, 2, 5, 7, 8];
 
 console.log(solution(a, b));
