b7e58296a5a8a24526bd0586a5c859eb9358fa4e
parkgeonwoong <hk77kh@gmail.com>
2023-03-30 14:50:19 +0900

    Feat : ~ 아나그램 (내가 푼 방법)

--- test.js
+++ test.js
@@ -1,37 +1,19 @@
-/**
- * N개의 수로 이루어진 수열이 주어집니다.
-이 수열에서 연속부분수열의 합이 특정숫자 M이하가 되는 경우가 몇 번 있는지 구하는 프로그램을 작성하세요.
-만약 N=5, M=5이고 수열이 다음과 같다면 1 3 1 2 3
-합이 5이하가 되는 연속부분수열은 {1}, {3}, {1}, {2}, {3}, {1, 3}, {3, 1}, {1, 2}, {2, 3},
-{1, 3, 1}로 총 10가지입니다.
+function solution(a, b) {
+  let answer = "";
 
-- 순회돌고
-m < item 이하 1개씩, 2개씩, 3개씩...
+  const sortA = a.split("").sort().join("");
+  const sortB = b.split("").sort().join("");
 
- */
-
-function solution(m, arr) {
-  let answer = 0;
-  let max = Number.MIN_SAFE_INTEGER;
-  let left = 0;
-  let sum = 0;
-
-  for (let right = 0; right < arr.length; right++) {
-    sum += arr[right];
-
-    if (right - left === m - 1) {
-      if (sum > max) max = sum;
-
-      while (right - left >= m - 1) {
-        sum -= arr[left++];
-      }
-    }
-    answer = max;
-  }
+  sortA.includes(sortB) ? (answer = "YES") : (answer = "NO");
 
   return answer;
 }
+let a = "AbaAeCe";
+let b = "baeeACA";
 
-let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+console.log(solution(a, b));
 
-console.log(solution(3, a));
+let c = "abaCC";
+let d = "Caaab";
+
+console.log(solution(c, d));
