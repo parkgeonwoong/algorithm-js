3aec025c4eec6c6ff6d08efb628024b054514f6b
parkgeonwoong <hk77kh@gmail.com>
2023-03-30 19:53:09 +0900

    Feat : 학급회장 (obj, Map key value 가져오는법 중요)

--- codingtest/efficiency/06학급회장.js
+++ codingtest/efficiency/06학급회장.js
@@ -3,6 +3,16 @@
  * 투표용지에는 반 학생들이 자기가 선택한 후보의 기호(알파벳)가 쓰여져 있으며 선생님은 그 기호를 발표하고 있습니다.
  * 선생님의 발표가 끝난 후 어떤 기호의 후보가 학급 회장이 되었는지 출력하는 프로그램을 작성하세요.
  * 반드시 한 명의 학급회장이 선출되도록 투표결과가 나왔다고 가정합니다.
+ *
+ * @푼방법
+ * - 객체를 만들어서 (중복이 안되니까) 추가해주는 방식
+ * key값을 가져오는 방식
+ * 1. for ..in 키를 가지고 옴
+ * 2. Object.keys().reduce() 사용
+ *
+ * @다른방식
+ * - Map을 사용한 객체
+ * for (let [key, value] of map)
  */
 
 // 내가 푼 방법
@@ -23,6 +33,30 @@ function solution(str) {
   }
 
   // console.log(Object.keys(obj).reduce((a, b) => (obj[a] > obj[b] ? a : b)));
+
+  return answer;
+}
+
+// Map 구현
+function solution(str) {
+  let answer = "";
+  let map = new Map();
+  let max = Number.MIN_SAFE_INTEGER;
+
+  for (let x of str) {
+    if (!map.has(x)) {
+      map.set(x, 1);
+    } else {
+      map.set(x, map.get(x) + 1);
+    }
+  }
+
+  for (let [key, value] of map) {
+    if (value > max) {
+      max = value;
+      answer = key;
+    }
+  }
 
   return answer;
 }
--- test.js
+++ test.js
@@ -1,17 +1,26 @@
-function solution(m, arr) {
-  let answer = 0;
-  let sum = 0;
+function solution(str) {
+  let answer = "";
+  let map = new Map();
+  let max = Number.MIN_SAFE_INTEGER;
 
-  for (let i = 0; i < m; i++) sum += arr[i];
-  answer = sum;
-
-  for (let i = m; i < arr.length; i++) {
-    sum += arr[i] - arr[i - m];
-    answer = Math.max(answer, sum);
+  for (let x of str) {
+    if (!map.has(x)) {
+      map.set(x, 1);
+    } else {
+      map.set(x, map.get(x) + 1);
+    }
   }
+
+  for (let [key, value] of map) {
+    if (value > max) {
+      max = value;
+      answer = key;
+    }
+  }
+
   return answer;
 }
 
-let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+let str = "BACBACCACCBDEDE";
 
-console.log(solution(3, a));
+console.log(solution(str));
