9921fade67f897b52af3d0e8cb5f28fba5623313
parkgeonwoong <hk77kh@gmail.com>
2023-04-03 22:44:26 +0900

    Feat : LRU 알고리즘 선택정렬로 구현

--- test.js
+++ test.js
@@ -1,18 +1,27 @@
-function solution(str) {
-  for (let i = 1; i < str.length; i++) {
-    let temp = str[i];
-    let j = i;
+function solution(n, arr) {
+  let answer = Array.from({ length: n }, () => 0);
 
-    while (j > 0 && str[j - 1] > temp) {
-      str[j] = str[j - 1];
-      j--;
+  arr.forEach((x) => {
+    let pos = -1;
+
+    // hit, miss 판별부터
+    for (let i = 0; i < n; i++) if (x === answer[i]) pos = i;
+
+    // 없으면 맨앞에 넣어주고 miss
+    if (pos === -1) {
+      answer.unshift(x);
+      if (answer.length > n) answer.pop();
     }
 
-    str[j] = temp;
-  }
+    // 있으면 잘라내고, 맨앞에다가 배치 hit
+    else {
+      answer.splice(pos, 1);
+      answer.unshift(x);
+    }
+  });
 
-  return str;
+  return answer;
 }
 
-let arr = [11, 7, 5, 6, 10, 9];
-console.log(solution(arr));
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+console.log(solution(5, arr));
