0f73f53c368a6c94433118d728bbc44f11545b2d
parkgeonwoong <hk77kh@gmail.com>
2023-04-04 15:31:47 +0900

    Feat : ~ 좌표 정렬

--- test.js
+++ test.js
@@ -1,27 +1,12 @@
-function solution(n, arr) {
-  let answer = Array.from({ length: n }, () => 0);
-
-  arr.forEach((x) => {
-    let pos = -1;
-
-    // hit, miss 판별부터
-    for (let i = 0; i < n; i++) if (x === answer[i]) pos = i;
-
-    // 없으면 맨앞에 넣어주고 miss
-    if (pos === -1) {
-      answer.unshift(x);
-      if (answer.length > n) answer.pop();
-    }
-
-    // 있으면 잘라내고, 맨앞에다가 배치 hit
-    else {
-      answer.splice(pos, 1);
-      answer.unshift(x);
-    }
-  });
-
-  return answer;
+function solution(arr) {
+  return arr.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
 }
 
-let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
-console.log(solution(5, arr));
+let arr = [
+  [2, 7],
+  [1, 3],
+  [1, 2],
+  [2, 5],
+  [3, 6],
+];
+console.log(solution(arr));
