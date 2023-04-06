14bddd10e786bafc9d270f2f20d61cee629210b5
parkgeonwoong <hk77kh@gmail.com>
2023-04-06 15:35:41 +0900

    Feat : 이분검색 구현

--- test.js
+++ test.js
@@ -1,31 +1,20 @@
-function solution(arr) {
-  let answer = 0;
-  let newArr = [];
+function solution(target, arr) {
+  let answer;
+  arr.sort((a, b) => a - b);
+  let left = 0;
+  let right = arr.length - 1;
 
-  for (let x of arr) {
-    newArr.push([x[0], "start"]);
-    newArr.push([x[1], "end"]);
-  }
-
-  newArr.sort((a, b) => (a[0] === b[0] ? (a[1] > b[1] ? 1 : -1) : a[0] - b[0]));
-
-  console.log(newArr);
-
-  let cnt = 0;
-  for (let x of newArr) {
-    if (x[1] === "start") cnt++;
-    else cnt--;
-    answer = Math.max(answer, cnt);
+  while (left <= right) {
+    let mid = parseInt((left + right) / 2);
+    if (arr[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (arr[mid] > target) right = mid - 1;
+    else left = mid + 1;
   }
 
   return answer;
 }
 
-let arr = [
-  [14, 18],
-  [12, 15],
-  [15, 20],
-  [20, 30],
-  [5, 14],
-];
-console.log(solution(arr));
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+console.log(solution(32, arr));
