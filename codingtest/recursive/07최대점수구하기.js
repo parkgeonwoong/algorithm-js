fcccf2d74253a069a52f84f608edd506df194d9d
parkgeonwoong <hk77kh@gmail.com>
2023-04-07 22:19:55 +0900

    Feat : ~ 중복순열 구하기 (이진트리 DFS)

---
+++ codingtest/recursive/07최대점수구하기.js
@@ -0,0 +1,31 @@
+/**
+ * 이번 정보올림피아드대회에서 좋은 성적을 내기 위하여 현수는 선생님이 주신 N개의 문제를 풀려고 합니다.
+ * 각 문제는 그것을 풀었을 때 얻는 점수와 푸는데 걸리는 시간이 주어지게 됩니다.
+ * 제한시간 M안에 N개의 문제 중 최대점수를 얻을 수 있도록 해야 합니다.
+ * (해당문제는 해당시간이 걸리면 푸는 걸로 간주한다, 한 유형당 한개만 풀 수 있습니다.)
+ */
+
+function solution(m, score, time) {
+  let answer = 0;
+  let n = score.length;
+
+  function DFS(Level, scoreSum, timeSum) {
+    if (m < timeSum) return;
+
+    if (Level === n) {
+      answer = Math.max(answer, scoreSum);
+    }
+    //
+    else {
+      DFS(Level + 1, scoreSum + score[Level], timeSum + time[Level]);
+      DFS(Level + 1, scoreSum, timeSum);
+    }
+  }
+
+  DFS(0, 0, 0);
+  return answer;
+}
+
+let score = [10, 25, 15, 6, 7];
+let time = [5, 12, 8, 3, 4];
+console.log(solution(20, score, time));
--- codingtest/recursive/08중복순열구하기.js
+++ codingtest/recursive/08중복순열구하기.js
@@ -1,3 +1,7 @@
+/**
+ * 1부터 N까지 번호가 적힌 구슬이 있습니다. 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열하는 방법을 모두 출력합니다.
+ */
+
 function solution(n, m) {
   let answer = [];
   let temp = Array.from({ length: m }, () => 0);
