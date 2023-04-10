a21cf7485429d97569c373a599b9811b48eefcbf
parkgeonwoong <hk77kh@gmail.com>
2023-04-10 14:05:38 +0900

    Feat : ~ 수열 추측하기 (조합과 순열)

--- codingtest/recursive/12조합의경우수.js
+++ codingtest/recursive/12조합의경우수.js
@@ -20,4 +20,23 @@ function solution(n, c) {
   return answer;
 }
 
+// 메모이제이션
+function solution(n, r) {
+  let answer = 0;
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
+
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+
+    if (n === r || r === 0) return 1;
+    else {
+      return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+    }
+  }
+
+  answer = DFS(n, r);
+
+  return answer;
+}
+
 console.log(solution(33, 19));
