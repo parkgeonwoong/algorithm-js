8e4d339357451979933b7530e2be76d12d38c15c
parkgeonwoong <hk77kh@gmail.com>
2023-04-11 11:46:01 +0900

    Feat : 이진트리 (BFS) 구현

--- codingtest/graph/04이진트리넓이우선탐색.js
+++ codingtest/graph/04이진트리넓이우선탐색.js
@@ -1,3 +1,11 @@
+/**
+ * 1로 시작하는 이진트리를 넓이우선탐색해 보세요
+ *
+ * @설명
+ * BFS는 상태트리로 최단거리를 구할때 쓰임
+ * 레벨 탐색이 핵심
+ */
+
 function solution() {
   let answer = "";
   let queue = [];
@@ -6,6 +14,7 @@ function solution() {
   while (queue.length) {
     let v = queue.shift();
     answer += v + " ";
+
     for (let next of [v * 2, v * 2 + 1]) {
       if (next > 7) continue;
       queue.push(next);
@@ -14,4 +23,5 @@ function solution() {
 
   return answer;
 }
+
 console.log(solution());
