b64b8437a178b2de63d94977d0ec55e5c9f7a8b9
parkgeonwoong <hk77kh@gmail.com>
2023-04-12 14:04:56 +0900

    Feat : 섬나라 아일랜드 (BFS)

--- test.js
+++ test.js
@@ -3,25 +3,31 @@ function solution(board) {
   let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
   let dy = [0, 1, 1, 1, 0, -1, -1, -1];
   let n = board.length;
+  let queue = [];
 
-  function DFS(x, y) {
-    board[x][y] = 0;
-    for (let k = 0; k < 8; k++) {
-      let nx = x + dx[k];
-      let ny = y + dy[k];
-
-      if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
-        DFS(nx, ny);
-      }
-    }
-  }
-
+  // BFS
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
       if (board[i][j] === 1) {
         board[i][j] = 0;
+        queue.push([i, j]);
         answer++;
-        DFS(i, j);
+
+        while (queue.length) {
+          let [x, y] = queue.shift();
+          console.log(x, y);
+
+          for (let k = 0; k < 8; k++) {
+            let nx = x + dx[k];
+            let ny = y + dy[k];
+
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+              board[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
+        console.log("BFS end");
       }
     }
   }
