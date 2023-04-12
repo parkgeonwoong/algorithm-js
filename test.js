a5f2c970893f44767913a6b2f3caa8482c07fd26
parkgeonwoong <hk77kh@gmail.com>
2023-04-12 15:12:34 +0900

    Feat : 계단 오르기 (다이나믹)

--- test.js
+++ test.js
@@ -1,48 +1,15 @@
-function solution(board) {
+function solution(n) {
   let answer = 0;
-  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
-  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
-  let n = board.length;
-  let queue = [];
+  let dy = Array.from({ length: n + 1 }, () => 0);
 
-  // BFS
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (board[i][j] === 1) {
-        board[i][j] = 0;
-        queue.push([i, j]);
-        answer++;
-
-        while (queue.length) {
-          let [x, y] = queue.shift();
-          console.log(x, y);
-
-          for (let k = 0; k < 8; k++) {
-            let nx = x + dx[k];
-            let ny = y + dy[k];
-
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
-              board[nx][ny] = 0;
-              queue.push([nx, ny]);
-            }
-          }
-        }
-        console.log("BFS end");
-      }
-    }
+  dy[1] = 1;
+  dy[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dy[i] = dy[i - 2] + dy[i - 1];
   }
+  answer = dy[n];
 
   return answer;
 }
 
-let arr = [
-  [1, 1, 0, 0, 0, 1, 0],
-  [0, 1, 1, 0, 1, 1, 0],
-  [0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 0, 1, 1],
-  [1, 1, 0, 1, 1, 0, 0],
-  [1, 0, 0, 0, 1, 0, 0],
-  [1, 0, 1, 0, 1, 0, 0],
-];
-
-console.log(solution(arr));
+console.log(solution(7));
