a1f82cd9dba55e93c89c404c7886b7672382dcb2
parkgeonwoong <hk77kh@gmail.com>
2023-04-17 14:20:05 +0900

    Feat : n,m 2차원 입력값 만들기

--- baekjoon/브론즈/브론즈5/02행렬덧셈.js
+++ baekjoon/브론즈/브론즈5/02행렬덧셈.js
@@ -1,3 +1,19 @@
+/**
+ * 2738번
+ *
+ * @생각
+ * 2차원 입출력 배열을 n,m을 받아서 이어서 나오는것이 어려웠다.
+ * 
+ * @입력
+3 3
+1 1 1
+2 2 2
+0 1 0
+3 3 3
+4 4 4
+5 5 100
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
 const input = require("fs")
@@ -23,11 +39,4 @@ for (let i = 0; i < n; i++) {
 }
 
 console.log("result: ", array);
-
 console.log(array.map((v) => v.join(" ")).join("\n"));
-
-// function solution(n) {
-//   let answer = n.reduce((a, b) => a + Math.pow(b, 2), 0);
-//   return answer % 10;
-// }
-// console.log(solution(input));
