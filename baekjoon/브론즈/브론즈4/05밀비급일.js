b72d5b2c20b487effa0f4e1dc7f5758438bf26ed
parkgeonwoong <hk77kh@gmail.com>
2023-04-23 11:45:23 +0900

    Feat : 밀비급일

--- baekjoon/브론즈/브론즈4/05밀비급일.js
+++ baekjoon/브론즈/브론즈4/05밀비급일.js
@@ -1,3 +1,7 @@
+/**
+ * 11365
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
@@ -6,8 +10,19 @@ const input = require("fs")
   .split("\n")
   .map((v) => v.split(""));
 
-// console.log(input);
+console.log(input);
 
+function solution(s) {
+  s = s.slice(0, -1);
+  let answer = [];
+  for (let x of s) {
+    answer.push(x.reverse().join(""));
+  }
+
+  return answer.join("\n");
+}
+
+// 한줄로 단축
 function solution(s) {
   return s
     .slice(0, -1)
