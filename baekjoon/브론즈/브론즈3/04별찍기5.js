26acceca14b6cc1d311a9547fcb2e77cb61ce58d
parkgeonwoong <hk77kh@gmail.com>
2023-04-26 12:52:38 +0900

    Feat : 별찍기5

--- baekjoon/브론즈/브론즈3/04별찍기5.js
+++ baekjoon/브론즈/브론즈3/04별찍기5.js
@@ -1,3 +1,7 @@
+/**
+ * 2442
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
@@ -8,6 +12,21 @@ const input = require("fs")
 
 console.log(input);
 
+function solution(n) {
+  let answer = [];
+  let end = 2 * n - 1;
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+    let cnt = 2 * i - 1;
+    for (let j = 0; j < (end - cnt) / 2; j++) str += " ";
+    for (let k = 0; k < cnt; k++) str += "*";
+    for (let j = 0; j < (end - cnt) / 2; j++) str += " ";
+    answer.push(str);
+  }
+  return answer.join("\n");
+}
+
+// 다른 풀이
 function solution(n) {
   let answer = "";
   for (let i = 1; i <= n; i++) {
