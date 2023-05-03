53d8250d7a5f79541bbf4d4848a4beb32b3559b7
parkgeonwoong <hk77kh@gmail.com>
2023-05-03 13:45:18 +0900

    Feat : 별찍기 9,7

--- baekjoon/브론즈/브론즈3/06별찍기9.js
+++ baekjoon/브론즈/브론즈3/06별찍기9.js
@@ -1,3 +1,7 @@
+/**
+ * 2446
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = Number(require("fs").readFileSync(fileName).toString().trim());
 
@@ -5,10 +9,10 @@ console.log(input);
 
 function solution(n) {
   let answer = "";
-  for (let i = 1; i <= n; i++) {
+  for (let i = n; i >= 1; i--) {
     answer += " ".repeat(n - i) + "*".repeat(2 * i - 1) + "\n";
   }
-  for (let i = n - 1; i >= 1; i--) {
+  for (let i = 2; i <= n; i++) {
     answer += " ".repeat(n - i) + "*".repeat(2 * i - 1) + "\n";
   }
   return answer;
