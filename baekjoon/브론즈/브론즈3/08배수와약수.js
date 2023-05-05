04941c0f0727097336c12e7d53c16cd6a22c8013
parkgeonwoong <hk77kh@gmail.com>
2023-05-05 15:52:02 +0900

    Feat : 배수와 약수

--- baekjoon/브론즈/브론즈3/08배수와약수.js
+++ baekjoon/브론즈/브론즈3/08배수와약수.js
@@ -1,3 +1,8 @@
+/**
+ * 5086
+ */
+
+// 내가 푼 방법
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
@@ -10,6 +15,21 @@ const input = require("fs")
 console.log(input);
 
 function solution(n) {
+  let answer = [];
+  n.forEach((v) => {
+    if (v[0] < v[1]) {
+      if (v[1] % v[0] === 0) answer.push("factor");
+      else answer.push("neither");
+    } else {
+      if (v[0] % v[1] === 0) answer.push("multiple");
+      else answer.push("neither");
+    }
+  });
+  return answer.join("\n");
+}
+
+// 리팩토링이 가능할 것 같애서 진행
+function solution(n) {
   let answer = n.map(([a, b]) => {
     if (b % a === 0) return "factor";
     if (a % b === 0) return "multiple";
@@ -17,4 +37,5 @@ function solution(n) {
   });
   return answer.join("\n");
 }
+
 console.log(solution(input));
