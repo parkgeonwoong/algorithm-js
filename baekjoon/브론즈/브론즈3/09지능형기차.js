6419efb7f9220341fc30526e2f615f5847ea4427
parkgeonwoong <hk77kh@gmail.com>
2023-05-07 15:06:23 +0900

    Feat : 지능형기차

--- baekjoon/브론즈/브론즈3/09지능형기차.js
+++ baekjoon/브론즈/브론즈3/09지능형기차.js
@@ -1,3 +1,10 @@
+/**
+ * 2455
+ *
+ * answer은 최댓값 인원수를 구해야하기 때문에 새로운 변수를 만들어서 max값을 구해야 한다.
+ * answer로 max를 비교하면 안됨
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
