f8a29fce452e53b24b7761e52f41284de8a14b35
parkgeonwoong <hk77kh@gmail.com>
2023-05-24 12:00:48 +0900

    Feat : 집합의 포함여부 O(1)

--- baekjoon/실버/실버4/01수찾기.js
+++ baekjoon/실버/실버4/01수찾기.js
@@ -1,3 +1,9 @@
+/**
+ * 1920
+ * 시간초과
+ */
+
+// 시간초과
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
@@ -7,6 +13,19 @@ const input = require("fs")
   .map((v) => v.split(" ").map(Number));
 
 console.log(input);
+
+function solution(arr) {
+  let answer = [];
+  const [, N, , M] = arr;
+  for (let x of M) {
+    N.includes(x) ? answer.push(1) : answer.push(0);
+  }
+  return answer.join("\n");
+}
+
+console.log(solution(input));
+
+// Set으로 품
 
 function solution(arr) {
   const [, N, , M] = arr;
@@ -19,5 +38,3 @@ function solution(arr) {
 
   return answer.join("\n");
 }
-
-console.log(solution(input));
