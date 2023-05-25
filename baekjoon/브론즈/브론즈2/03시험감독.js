ed1cb915e71ff886102b6f3793f77bb768ac51e2
parkgeonwoong <hk77kh@gmail.com>
2023-05-25 11:53:16 +0900

    Feat : 시험감독

--- baekjoon/브론즈/브론즈2/03시험감독.js
+++ baekjoon/브론즈/브론즈2/03시험감독.js
@@ -1,3 +1,9 @@
+/**
+ * 13458
+ * 1. 각 방마다 총감독이 무조건 1명씩 맡는거였음. 글은 친절하지 않다는 점 기억.
+ * 2. 각 방마다 총감독보다 작을 수 있는 예외처리 할 것 (음수가 나오면 곤란)
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
