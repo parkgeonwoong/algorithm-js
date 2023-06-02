69a0ceb19048bd4e640ae4cbc1ef44b8288356b5
parkgeonwoong <hk77kh@gmail.com>
2023-06-02 14:57:06 +0900

    Feat : 스택 Obj 구현

--- baekjoon/실버/실버4/05스택.js
+++ baekjoon/실버/실버4/05스택.js
@@ -1,3 +1,18 @@
+/**
+ * 10828
+ * ✅ 굉장히 괜찮은 문제!
+ * [첫번째 생각]
+ * 1. 순수 if, else if, else로 구현할지 생각
+ *
+ * [두번째 생각]
+ * 1. Object 함수로 적용해보자 (새로운 방법으로 풀고싶음)
+ *
+ * NOTE:
+ * 1. stack에다가 값을 담지만 return하는것이 아닌데 실수. -> answer 배열 새로 만듬
+ *  ∴ push 빼고 호출이라서 값을 담아줬다.
+ * 2. obj 말고 input의 for문이 오히려 핵심이다.
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
