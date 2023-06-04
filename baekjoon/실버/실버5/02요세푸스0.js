b34e84a617103d63d221a9d4e50ceee90f1f48fb
parkgeonwoong <hk77kh@gmail.com>
2023-06-04 14:39:33 +0900

    Feat : 요세푸스 (큐의 순서)

--- baekjoon/실버/실버5/02요세푸스0.js
+++ baekjoon/실버/실버5/02요세푸스0.js
@@ -1,3 +1,9 @@
+/**
+ * 11866
+ * 처음에는 인덱스와 카운터로 구할려고 했으나 이해하기 어려운방식
+ * 앞에서부터 맨뒤로 옮기면서 이중for문으로 가야한다.
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
