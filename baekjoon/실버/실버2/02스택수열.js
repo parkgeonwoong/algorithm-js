8781d4149b6c119496ad6abd309eda7c6cc9523d
parkgeonwoong <hk77kh@gmail.com>
2023-06-08 15:01:09 +0900

    Feat : 스택수열

--- baekjoon/실버/실버2/02스택수열.js
+++ baekjoon/실버/실버2/02스택수열.js
@@ -1,3 +1,11 @@
+/**
+ * 1874
+ *
+ * 1. 스택에 넣고 빼서 수열을 만듬
+ * 2. 넣을 때 오름차순
+ * 3. 입력을 만들 수 있는가?
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
