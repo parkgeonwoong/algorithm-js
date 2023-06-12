fa06f3d090316eec4c5ebd787d326f1104f3c8c5
parkgeonwoong <hk77kh@gmail.com>
2023-06-12 12:31:26 +0900

    Feat : 세로읽기

--- baekjoon/브론즈/브론즈1/07세로읽기.js
+++ baekjoon/브론즈/브론즈1/07세로읽기.js
@@ -1,3 +1,14 @@
+/**
+ * 10798
+ *
+ * 1. 세로로 읽기
+ * 2. 없다면 공백처리
+ *
+ * 1. 가로 1번에 세로는 전체를 순회한다. 그러므로 for문을 반대로 생각
+ * 2. 배열중 최대길이를 찾는다.
+ * 3. answer에 붙일때 공백이면 undefined인데 || '' 이것으로 붙힌다.
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
