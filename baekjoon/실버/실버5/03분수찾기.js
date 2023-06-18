4014e9f52c265d27a9a38ea46db85ad981d3b50b
parkgeonwoong <hk77kh@gmail.com>
2023-06-18 13:13:28 +0900

    Feat : 분수찾기 (그리드 접근, 예외처리)

--- baekjoon/실버/실버5/03분수찾기.js
+++ baekjoon/실버/실버5/03분수찾기.js
@@ -1,3 +1,7 @@
+/**
+ * 1193
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = Number(require("fs").readFileSync(fileName).toString().trim());
 
@@ -8,12 +12,14 @@ function solution(n) {
   const copyN = n;
   let i = 0;
 
+  // 주어진 입력의 구간 배열로 만들기
   while (n > 0) {
     i++;
     arr.push(i);
     n -= i;
   }
 
+  // 배열에서 몇번째인지 구하기, 지그재그로 순서가 바뀜 (짝수, 홀수번호)
   const x = arr.reduce((a, b) => a + b, 0) - copyN;
   const left = i % 2 === 0 ? arr[arr.length - 1 - x] : arr[x];
   const right = i % 2 === 0 ? arr[x] : arr[arr.length - 1 - x];
