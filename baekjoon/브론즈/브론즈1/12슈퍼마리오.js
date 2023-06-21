410af7010ff47ee78f603900d7eff98be05ddc41
parkgeonwoong <hk77kh@gmail.com>
2023-06-21 21:53:12 +0900

    Feat : 슈퍼마리오 (접근하는방법)

--- baekjoon/브론즈/브론즈1/12슈퍼마리오.js
+++ baekjoon/브론즈/브론즈1/12슈퍼마리오.js
@@ -1,3 +1,12 @@
+/**
+ * 2851
+ *
+ * 접근
+ * - 100에 가까운 값을 도출하기 위해서 어떤 생각을 해야하는가?
+ * - |100-x|의 최소값을 구해야함
+ * - 순회를 돌면서 값을 넣기 전과 넣은 후의 값을 비교해야함
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(fileName)
