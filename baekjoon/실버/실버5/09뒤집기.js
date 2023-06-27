902a4571fadaa290f002bcc54d846d660aaaed86
parkgeonwoong <hk77kh@gmail.com>
2023-06-27 16:49:53 +0900

    baek : 뒤집기 (로직이 기억에 남아서 기록)

--- baekjoon/실버/실버5/09뒤집기.js
+++ baekjoon/실버/실버5/09뒤집기.js
@@ -1,3 +1,7 @@
+/**
+ * 1439
+ */
+
 const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs").readFileSync(fileName).toString().trim().split("");
 
@@ -11,7 +15,7 @@ function solution(input) {
   let temp = "";
 
   for (let i = 0; i < s.length - 1; i++) {
-    temp += s[i];
+    temp += s[i]; // [fix] 중복되서 요약한 것
     if (s[i] !== s[i + 1]) {
       s[i] === "0" ? arr0.push(temp) : arr1.push(temp);
       temp = "";
