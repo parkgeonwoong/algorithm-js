be50b94ae9d802474052ce425f2f4376e5ee649f
Woongs <65653053+parkgeonwoong@users.noreply.github.com>
2023-06-30 13:15:02 +0900

    [level 1] Title: 소수 찾기, Time: 97.62 ms, Memory: 42.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,21 @@
+function solution(n) {
+  let answer = 0;
+  const arr = Array.from({ length: n + 1 }, () => true); // 모두 소수라고 가정
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (arr[i] === false) {
+      continue;
+    }
+
+    // 합성수 2는 소수이므로 4는 소수가 아니다.
+    for (let j = i * i; j <= n; j += i) {
+      arr[j] = false;
+    }
+  }
+
+  // 2부터 0,1은 세지않음
+  for (let i = 2; i <= n; i++) {
+    if (arr[i]) answer++;
+  }
+  return answer;
+}
