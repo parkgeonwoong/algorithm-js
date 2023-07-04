27624bf3d96f6ea83846d4d4abaeb217ddb5abc2
Woongs <65653053+parkgeonwoong@users.noreply.github.com>
2023-07-04 14:55:11 +0900

    [Silver V] Title: 피보나치 수 4, Time: 220 ms, Memory: 16380 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.js
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.js
@@ -2,15 +2,21 @@ const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = +require("fs").readFileSync(fileName).toString().trim();
 
 function solution(input) {
-  const memo = [0, 1];
+  const memo = [0n, 1n];
 
-  function fibo(n) {
-    if (memo[n] !== undefined) return memo[n];
-    memo[n] = BigInt(fibo(n - 1)) + BigInt(fibo(n - 2));
-    return memo[n];
+  for (let i = 2; i <= input; i++) {
+    memo[i] = BigInt(memo[i - 1] + memo[i - 2]);
   }
 
-  return fibo(input).toString();
+  return memo[input].toString();
 }
 
 console.log(solution(input));
+
+// function fibo(n) {
+//   if (memo[n] !== undefined) return memo[n];
+//   memo[n] = BigInt(fibo(n - 1)) + BigInt(fibo(n - 2));
+//   return memo[n];
+// }
+
+// return fibo(input).toString();
