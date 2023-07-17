d28180bcc5d8bad626a41bd13bff42a05d1e415d
Woongs <65653053+parkgeonwoong@users.noreply.github.com>
2023-07-17 14:55:43 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.69 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,9 @@
+function solution(A,B){
+    A.sort((a,b) => a-b);
+    B.sort((a,b) => b-a);
+    
+    return A.reduce((acc, _, i) => {
+        acc += A[i] * B[i]
+        return acc;
+    }, 0)
+}
