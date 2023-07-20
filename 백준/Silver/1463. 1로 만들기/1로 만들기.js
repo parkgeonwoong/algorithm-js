ef76a026ff9ba0e1febf8cd693e3035734786ee1
Woongs <65653053+parkgeonwoong@users.noreply.github.com>
2023-07-20 16:06:03 +0900

    [Silver III] Title: 1로 만들기, Time: 284 ms, Memory: 17500 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,21 @@
+const fileName = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = +require("fs").readFileSync(fileName).toString().trim();
+
+function solution(n) {
+  const dp = Array.from({ length: n + 1 }, () => 0);
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + 1; // 점화식 기본값: before+1
+
+    if (i % 2 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    }
+
+    if (i % 3 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+    }
+  }
+  return dp[n];
+}
+
+console.log(solution(input));
