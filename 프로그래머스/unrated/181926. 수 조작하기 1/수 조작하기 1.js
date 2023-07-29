f74caf20969e14a0f0da711e8f56a812ac5768cd
Woongs <65653053+parkgeonwoong@users.noreply.github.com>
2023-07-29 20:17:27 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.93 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -2,20 +2,11 @@ function solution(n, control) {
     
     for(let x of control.split('')) {
         switch(x) {
-            case 'w':
-                n++;
-                break;
-            case 's':
-                n--;
-                break;
-            case 'd':
-                n+=10;
-                break;
-            case 'a':
-                n-=10;
-                break;
-            default:
-                break;
+            case 'w': n++;   break;
+            case 's': n--;   break;
+            case 'd': n+=10; break;
+            case 'a': n-=10; break;
+            default:  break;
         }
     }
     return n;
