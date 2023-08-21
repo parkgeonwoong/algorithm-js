8a888235a30fa4d3e24bb76881ffc007a5379001
Woongs <65653053+parkgeonwoong@users.noreply.github.com>
2023-08-21 15:11:50 +0900

    [Silver III] Title: 수열, Time: 220 ms, Memory: 23776 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.js
+++ 백준/Silver/2559. 수열/수열.js
@@ -8,21 +8,40 @@ const input = require("fs")
 
 function solution(input) {
   const [[N, K], [...arr]] = input;
-  const answer = [];
+  const sum = [0];
 
-  // 0~연속되는 마지막까지
-  for (let i = 0; i <= N - K; i++) {
-    let j = i;
-    let temp = 0;
-
-    // K(연속되는 몇일)까지 합
-    while (j < K + i) {
-      temp += arr[j++];
-    }
-    answer.push(temp);
+  // 누적합
+  for (let i = 1; i <= N; i++) {
+    sum[i] = sum[i - 1] + arr[i - 1];
   }
 
-  return Math.max(...answer);
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let i = K; i <= N; i++) {
+    max = Math.max(max, sum[i] - sum[i - K]);
+  }
+  return max;
 }
 
 console.log(solution(input));
+
+/**
+ * 풀었던 방법 -> 시간이 너무 큼
+ */
+// function solution(input) {
+//   const [[N, K], [...arr]] = input;
+//   const answer = [];
+
+//   // 0~연속되는 마지막까지
+//   for (let i = 0; i <= N - K; i++) {
+//     let j = i;
+//     let temp = 0;
+
+//     // K(연속되는 몇일)까지 합
+//     while (j < K + i) {
+//       temp += arr[j++];
+//     }
+
+//     answer.push(temp);
+//   }
+//   return Math.max(...answer);
+// }
